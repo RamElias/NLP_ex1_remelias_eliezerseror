1417fdc89e473f3ec5c9d93ea96f33dadf090e1a
Ram Elias <1ramelias10@gmail.com>
2022-11-21 14:30:32 +0200

    finish check words,  build matrix and   make hashteble

--- ex1.py
+++ ex1.py
@@ -1,68 +1,100 @@
 import re
-
-
-def marks_remove(text):
-    for i in range(len(text)):
-        arr = re.sub(r'[^\w\s]', '', text[i])
-        text[i] = arr
-        #print(text[i])
-
-
-def add_tokens(text):
-    for i in range(len(text)):
-        arr = text[i].split('\t')
-        text[i] = arr[0] + "<S>{0}</S>\n".format((arr[1].split('\n'))[0])
-        #print(text[i])
-
-
-def lower_case(text):
-    for i in range(len(text)):
-        arr = text[i].lower()
-        text[i] = arr
-        #print(text[i])
-
-
-def replace_numbers(text):
-    for i in range(len(text)):
-        arr = text[i].split('\t')
-        #rint(arr[0], "m")
-        text[i] = arr[0] + '\t'+ re.sub(r'([0-9]+)', '<!DIGIT!>', arr[1])
-        #print(text[i])
-
-
-def fix_words(text):
-    for i in range(len(text)):
-        arry = text[i].split(" ")
-        for j in range(len(arry)):
-            if not check_word(arry[j]):
-                arry[j].replace(arry[j],"<UNK>")
-        print(text[i])
-
-
-def check_word(word):
-    for i in range(len(word)):
-        if word[i] != (r'[a-zA-Z]'):
-            return False
-    return True
-
-
-
+import nltk
+import numpy as np
 
 
 def data_fixed(file):
     text = file.readlines()
-    marks_remove(text)
-    lower_case(text)
-    replace_numbers(text)
-    fix_words(text)
 
-    add_tokens(text)
+    for i in range(len(text)):
+        sentence = "<S>"
+        arr = text[i].split("\t")
+        word_arr = arr[1].split(" ")
 
+        for w in word_arr:
+            print(w)
+            sentence += check_word(w) + ' '
+
+        text[i] = sentence.strip() + "</S>\n"
+
+    return text
+
+
+def check_word(word):
+        word = re.sub(r'[^\w\s]', '', word)
+        word.lower()
+        word = re.sub(r'([0-9]+)', '<!DIGIT!>', word)
+        if re.search(r'[^\x00-\x7F]+', word):
+            return '<UNK>'
+
+        return word
+
+
+def create_matrix():
+    rows = get_rows()
+    cols = get_cols()
+
+    matrix = []
+    matrix.append(cols)
+
+    for i in rows:
+        matrix.append([i]+[""] * (len(cols)-1))
+
+    return matrix
+
+
+def get_rows():
+    rows_arr = []
+    simlex = open("EN-SIMLEX-999.txt", 'r', encoding="utf8")
+    rows = simlex.readlines()
+
+    for i in rows:
+        arr = i.split('\t')
+        for j in range(len(arr)-1):
+            if arr[j] in rows_arr:
+                continue
+            rows_arr.append(arr[j])
+
+    return rows_arr
+
+
+def get_cols():
+    cols_arr = []
+    freq = open("eng_wikipedia_2016_1M-words.txt", 'r', encoding="utf8")
+    cols = freq.readlines()
+    cols_arr.append("#")
+    for i in range(50, 20050):
+        arr = cols[i].split('\t')
+        cols_arr.append(arr[1])
+
+    return cols_arr
+
+
+def make_hash_table(text):
+    hash_t = {}
+    # file =  open('eng_wikipedia_2016_10K-sentences.txt', 'r', encoding="utf8")
+    # text = file.readlines()
+
+    for i, sentence in enumerate(text):
+        sentence = sentence[sentence.index("<S>") + len("<S>"):sentence.index("</S>") - 1].split(" ")
+
+        for j, word in enumerate(sentence):
+            if word in hash_t:
+                hash_t[word].append([i, j])
+            else:
+                hash_t[word] = [[i, j]]
+
+    return hash_t
 
 
 if __name__ == "__main__" :
-    file = open("eng_wikipedia_2016_10K-sentences.txt", 'r+', encoding="utf8")
-    data_fixed(file)
+    file = open("eng_wikipedia_2016_10K-sentences - Copy.txt", 'r+', encoding="utf8")
+    text = data_fixed(file)
+    print(text)
+    matrix = create_matrix()
+    matrix_hash = make_hash_table(text)
+    print(matrix_hash)
+
 
 
 
