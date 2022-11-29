dc93229864f18f7387f36484889f12f95c5ebe9a
Ram Elias <1ramelias10@gmail.com>
2022-11-29 10:11:37 +0200

    finished ppmi calculate

--- ex1.py
+++ ex1.py
@@ -1,6 +1,6 @@
 import re
-import nltk
 import numpy as np
+import math
 
 
 def data_fixed(file):
@@ -12,7 +12,6 @@ def data_fixed(file):
         word_arr = arr[1].split(" ")
 
         for w in word_arr:
-            print(w)
             sentence += check_word(w) + ' '
 
         text[i] = sentence.strip() + "</S>\n"
@@ -38,7 +37,7 @@ def create_matrix():
     matrix.append(cols)
 
     for i in rows:
-        matrix.append([i]+[""] * (len(cols)-1))
+        matrix.append([i]+[0] * (len(cols)-1))
 
     return matrix
 
@@ -63,7 +62,7 @@ def get_cols():
     freq = open("eng_wikipedia_2016_1M-words.txt", 'r', encoding="utf8")
     cols = freq.readlines()
     cols_arr.append("#")
-    for i in range(50, 20050):
+    for i in range(50, 100):
         arr = cols[i].split('\t')
         cols_arr.append(arr[1])
 
@@ -72,8 +71,6 @@ def get_cols():
 
 def make_hash_table(text):
     hash_t = {}
-    # file =  open('eng_wikipedia_2016_10K-sentences.txt', 'r', encoding="utf8")
-    # text = file.readlines()
 
     for i, sentence in enumerate(text):
         sentence = sentence[sentence.index("<S>") + len("<S>"):sentence.index("</S>") - 1].split(" ")
@@ -87,13 +84,57 @@ def make_hash_table(text):
     return hash_t
 
 
+def frequency_counts(matrix, matrix_hash, size):
+
+    for row in range(1, len(matrix)):
+        for col in range(1, len(matrix[0])):
+            matrix[row][col] = count(matrix[0][col], matrix[row][0], matrix_hash, size)
+    return matrix
+
+
+def count(simelx, wiki, matrix_hash, size):
+    counter = 0
+    if simelx in matrix_hash and wiki in matrix_hash:
+        for i in matrix_hash[simelx]:
+            for j in matrix_hash[wiki]:
+                if i[0] == j[0] and abs(i[1] - j[1]) <= size:
+                    counter += 1
+    return counter
+
+
+def calc_ppmi(frequency_matrix, hash_matrix):
+    amount_of_words = 0
+    for i in frequency_matrix:
+        amount_of_words += len(frequency_matrix[i])
+
+    ppmi_matrix = frequency_matrix
+
+    for row in range(1, len(ppmi_matrix)):
+        for col in range(1, len(ppmi_matrix[row])):
+            if ppmi_matrix[row][col] != 0:
+                numerator = (2+ppmi_matrix[row][col])/(amount_of_words + 2 * len(ppmi_matrix[0]) * len(ppmi_matrix))
+                denominator = (len(hash_matrix[ppmi_matrix[row][0]])/amount_of_words) * \
+                              (len(hash_matrix[ppmi_matrix[0][col]])/amount_of_words)
+                ppmi_matrix[row][col] = max(math.log2(numerator/denominator), 0)
+
+    return ppmi_matrix
+
+
 if __name__ == "__main__" :
-    file = open("eng_wikipedia_2016_10K-sentences - Copy.txt", 'r+', encoding="utf8")
+    file = open("eng_wikipedia_2016_10K-sentences.txt", 'r+', encoding="utf8")
     text = data_fixed(file)
-    print(text)
     matrix = create_matrix()
     matrix_hash = make_hash_table(text)
-    print(matrix_hash)
+
+    fc2 = frequency_counts(matrix, matrix_hash, 2)
+    fc5 = frequency_counts(matrix, matrix_hash, 5)
+
+    ppmi2 = calc_ppmi(fc2, matrix_hash)
+    ppmi5 = calc_ppmi(fc5, matrix_hash)
+    print(ppmi5)
+
+
+
 
 
 
